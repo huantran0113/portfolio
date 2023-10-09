c209d85bfcfe7580f4bffd8f14e2a9b48e3d2a27
huantran0113 <huantran0113@vt.edu>
2023-10-09 18:14:46 -0400

    test back to localhost

--- src/components/ContactMeModal.vue
+++ src/components/ContactMeModal.vue
@@ -38,7 +38,7 @@ async function sendEmail() {
   }
 
   axios
-    .post('https://app-mw7s.onrender.com/data', information)
+    .post('http://localhost:8080/data', information)
     .then((response) => {
       console.log(response.data.message)
     })
